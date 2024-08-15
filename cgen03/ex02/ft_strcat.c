d92d8ecd5c58c22f42d6f27c328140967d02ddd7
Miyachi Rio <mrio@c4r3s16.42tokyo.jp>
2024-08-15 21:54:39 +0900

    save

---
+++ cgen03/ex02/ft_strcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrio <mrio@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 11:53:11 by mrio              #+#    #+#             */
+/*   Updated: 2024/08/15 17:41:03 by mrio             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+// int	main(void)
+// {
+// 	char a[] = "ABC";
+// 	char b[] = "DEF";
+// 	char *result = ft_strcat(a, b);
+// 	printf("%s", result);
+// 	return (0);
+// }
---
+++ cgen03/ex05/ft_strlcat.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrio <mrio@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 16:42:45 by mrio              #+#    #+#             */
+/*   Updated: 2024/08/15 21:53:59 by mrio             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = 0;
+	src_len = 0;
+	while (dest[dest_len] != '\0')
+		dest_len++;
+	while (src[src_len] != '\0')
+		src_len++;
+	if (size == 0)
+		return (src_len);
+	if (dest_len >= size)
+		return (size + src_len);
+	i = 0;
+	while (src[i] != '\0' && dest_len + i < size - 1)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len - i);
+}
+
+// int	main(void)
+// {
+// 	char a[20] = "Hello";
+// 	char b[] = "World";
+// 	int size = 0;
+// 	int result = ft_strlcat(a, b, size);
+// 	printf("%d", result);
+// 	return (0);
+// }
