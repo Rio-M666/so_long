3961ab9876f3c1866c222e651287d88ab5382450
Miyachi Rio <mrio@c4r3s16.42tokyo.jp>
2024-08-15 20:27:41 +0900

    save

---
+++ cgen03-cp/ex03/ft_strncat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrio <mrio@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 13:40:03 by mrio              #+#    #+#             */
+/*   Updated: 2024/08/15 17:41:15 by mrio             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < nb && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+// int	main(void)
+// {
+// 	char a[] = "ABC";
+// 	char b[] = "DEF";
+// 	char *result = ft_strncat(a, b, 1);
+// 	printf("%s", result);
+// 	return (0);
+// }
---
+++ cgen03-cp/ex04/ft_strstr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrio <mrio@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/15 16:08:04 by mrio              #+#    #+#             */
+/*   Updated: 2024/08/15 17:41:23 by mrio             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	if (*to_find == '\0')
+	{
+		return (str);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && to_find[j] != '\0')
+		{
+			j++;
+		}
+		if (to_find[j] == '\0')
+		{
+			return (&str[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+// int	main(void)
+// {
+// 	char a[] = "Hello wozzd ! How are you?";
+// 	char b[] = "nvjs";
+// 	char *result = ft_strstr(a, b);
+// 	printf("%s", result);
+// 	return (0);
+// }
