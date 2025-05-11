30993e3bb5b292a3155b445e8d5e424778001487
RIo Miyachi <131347537+Rio-M666@users.noreply.github.com>
2025-05-11 09:43:24 +0900

    torima

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -3,66 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrio <mrio@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: miyachirio <miyachirio@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 19:26:24 by mrio              #+#    #+#             */
-/*   Updated: 2025/05/10 18:28:31 by mrio             ###   ########.fr       */
+/*   Updated: 2025/05/11 02:31:10 by miyachirio       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-
-
-void ft_check(va_list ap,char str)
+size_t	ft_check(va_list ap, char str)
 {
-    if(str=='s')
-        {
-            ft_putstr(va_arg(ap,char *));
-            
-        }
-    else if(str=='d')
-        ft_putnbr(va_arg(ap,int));
-    // else if(str=='i')
-    // else if(str=='c')
-    // else if(str=='p')
-    
-        
-    // else if(str=='%')
+	size_t	len;
 
-    
-    
-    
-        
+	len = 0;
+	if (str == 's')
+		return (ft_putstr(va_arg(ap, char *)));
+	else if (str == 'd' || str == 'i')
+		return (ft_putnbr(va_arg(ap, int)));
+	else if (str == 'c')
+		return (ft_putchr(va_arg(ap, int)));
+	else if (str == 'p')
+		return ();
+	else if (str == '%')
+		return (ft_putchr('%'));
+	return (0);
 }
 
 int	ft_printf(const char *str, ...)
 {
 	size_t	i;
-    size_t len;
+	size_t	len;
 	va_list	ap;
 
-
 	va_start(ap, str);
-    i=0;
+	i = 0;
+	len = 0;
 	while (str[i] != '\0')
 	{
 		if (str[i] != '%')
-			ft_putchr(str[i]);
+			len += ft_putchr(str[i]);
 		else
 		{
-            i++;
-			ft_check(ap,str[i]);
+			i++;
+			len += ft_check(ap, str[i]);
 		}
 		i++;
 	}
-    va_end(ap);
-	return len;
+	va_end(ap);
+	return (len);
 }
 
 int	main(void)
 {
-    char *str="adsd";
-	ft_printf("%s",str);
+	char *str = "adsd";
+	ft_printf("%s", str);
 	return (0);
 }
--- printf/libft.c
+++ printf/libft.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrio <mrio@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: miyachirio <miyachirio@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 16:50:16 by mrio              #+#    #+#             */
-/*   Updated: 2025/05/10 18:28:29 by mrio             ###   ########.fr       */
+/*   Updated: 2025/05/11 02:32:16 by miyachirio       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 size_t	ft_putchr(char c)
 {
-	write(1, &c, 1);
-	return (0);
+	return (write(1, &c, 1));
 }
 
-size_t ft_putstr(char *str)
+size_t	ft_putstr(char *str)
 {
-    int i;
-    i=0;
-    while(str[i])
-    {
-        ft_putchr(str[i]);
-        i++;
-    }
-    return i;
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchr(str[i]);
+		i++;
+	}
+	return (i);
 }
 
-size_t ft_putnbr(int c)
+size_t	ft_putnbr(int c)
 {
-    
+	long num;
 }
