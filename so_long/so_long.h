c883c5b2a4501fdbc9d10ba1c261f5033aaf0592
Miyachi Rio <mrio@c3r1s2.42tokyo.jp>
2025-09-07 06:41:42 +0900

    fin 2

--- so_long/so_long.h
+++ so_long/so_long.h
@@ -30,65 +30,64 @@
 
 typedef struct s_game
 {
-	void	*mlx;
-	void	*window;
-	char	**map;
-	int		map_width;
-	int		map_height;
-	int		player_x;
-	int		player_y;
-	int		collectibles_total;
-	int		collectibles_collected;
-	int		moves;
-	int		game_won;
-	int		player_on_exit;
-	void	*img_wall;
-	void	*img_player;
-	void	*img_exit;
-	void	*img_collectible;
-	void	*img_space;
-	void	*img_background;
+	void		*mlx;
+	void		*window;
+	char		**map;
+	int			map_width;
+	int			map_height;
+	int			player_x;
+	int			player_y;
+	int			collectibles_total;
+	int			collectibles_collected;
+	int			moves;
+	int			game_won;
+	int			player_on_exit;
+	void		*img_wall;
+	void		*img_player;
+	void		*img_exit;
+	void		*img_collectible;
+	void		*img_space;
+	void		*img_background;
 
-}			t_game;
+}				t_game;
 
-typedef struct s_checkdata
+typedef struct s_flood_data
 {
-	int		collectibles_found;
-	int		exit_found;
-	int		total_colectibles;
-	int		player_x;
-	int		player_y;
-	char	**map_copy;
-	int		player_count;
-	int		exit_count;
-	int		collectible_count;
-	int		x;
-	int		y;
-}			t_checkdata;
+	int			collectibles_found;
+	int			exit_found;
+	int			total_collectibles;
+}				t_flood_data;
 
-int			check_filename(char *filename);
-int			check_square(t_game *game);
-int			check_wall(t_game *game);
-int			check_character(t_game *game);
-int			check_path(t_game *game);
-int			count_elements(t_game *game, t_checkdata *data);
-int			find_player_position(t_game *game, t_checkdata *data);
-void		flood_fill(char **map_copy, int x, int y, t_checkdata *data);
+typedef struct s_element_count
+{
+	int			player_count;
+	int			exit_count;
+	int			collectible_count;
+}				t_element_count;
 
-void		init_mlx(t_game *game);
-void		cleanup_game(t_game *game);
-void		init_game_data(t_game *game, char **map);
-int			check_move(t_game *game, int x, int y);
-void		move_player(t_game *game, int x, int y);
-int			handle_key(int keycode, t_game *game);
-int			handle_close(t_game *game);
-void		load_assets(t_game *game);
-void		render_tile(t_game *game, char tile, int x, int y);
-void		render_map(t_game *game);
-int			validate_map(t_game *game);
-char		**read_map(char *filename);
-char		**copy_map(t_game *game);
-void		free_map(char **map);
-int			map_width(char **map);
-int			map_height(char **map);
+int				check_filename(char *filename);
+int				check_square(t_game *game);
+int				check_wall(t_game *game);
+int				check_character(t_game *game);
+int				check_path(t_game *game);
+t_element_count	count_elements(t_game *game);
+int				find_player_position(t_game *game);
+void			flood_fill(char **map_copy, int x, int y, t_flood_data *data);
+
+void			init_mlx(t_game *game);
+void			cleanup_game(t_game *game);
+int				init_game_data(t_game *game, char **map);
+int				check_move(t_game *game, int x, int y);
+void			move_player(t_game *game, int x, int y);
+int				handle_key(int keycode, t_game *game);
+int				handle_close(t_game *game);
+void			load_assets(t_game *game);
+void			render_tile(t_game *game, char tile, int x, int y);
+void			render_map(t_game *game);
+int				validate_map(t_game *game);
+char			**read_map(char *filename);
+char			**copy_map(t_game *game);
+void			free_map(char **map);
+int				map_width(char **map);
+int				map_height(char **map);
 #endif
